ff489fea3916c4b0a22c2b366a60a37a34aec09c
Prachi <prachikale136@gmail.com>
2023-06-07 23:11:09 +0530

    [PRachi] Add: check Employee present pr not

---
+++ EmployeeWageProblems/EmployeeWageProblems/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblems
+{
+    internal class Employee
+    {
+        public static void IsCheck()
+        {
+            int full_time = 1;
+            Random random= new Random();
+            int check = random.Next(0,2);
+
+            if(check == full_time)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else 
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+
+    }
+}
--- EmployeeWageProblems/EmployeeWageProblems/Program.cs
+++ EmployeeWageProblems/EmployeeWageProblems/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
+            Employee.IsCheck();
         }
     }
 }
