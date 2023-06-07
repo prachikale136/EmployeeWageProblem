5e4b71aef19169e7f215e6309764254ae0e66756
Prachi <prachikale136@gmail.com>
2023-06-07 23:46:26 +0530

    [Prachi] Add: Calculate the wages for a month

--- EmployeeWageProblems/EmployeeWageProblems/Employee.cs
+++ EmployeeWageProblems/EmployeeWageProblems/Employee.cs
@@ -12,27 +12,37 @@ namespace EmployeeWageProblems
         {
             const int Part_Time = 1;
             const int Full_Time = 2;
+            const int NumberOFWorkingDays = 20;
             int EmpWagePerHr= 20;
 
             int empHrs = 0;
-            int empWage = 0; 
-            Random random= new Random();
-            int check = random.Next(0,3);
-
-           switch(check)
+            int empWage = 0;
+            int  totalEmpWage = 0;
+            
+            for(int days =0; days < NumberOFWorkingDays; days++)
             {
-                case Full_Time:
-                       empHrs = 8;
-                    break;
-                case Part_Time:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int check = random.Next(0, 3);
+                switch (check)
+                {
+                    case Full_Time:
+                        empHrs = 8;
+                        break;
+                    case Part_Time:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EmpWagePerHr;
+                totalEmpWage += empWage;
+                Console.WriteLine(" Emp wage: " + empWage);
             }
-        empWage = empHrs * EmpWagePerHr;
-        Console.WriteLine("Emp wage: " + empWage);
+            Console.WriteLine("total emp wages: " +totalEmpWage);
+           
+        
+        
         }
 
     }
